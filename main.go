a08f92ffc8f65cdf8bd5bbcb022c2e3d56c09612
Matt Gleich <git@mattglei.ch>
2021-12-30 00:00:56 -0500

    feat: create tmp dir
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- main.go
+++ main.go
@@ -2,29 +2,33 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/gleich/lumber/v2"
-	"github.com/gleich/new_yearify/pkg/api"
-	"github.com/gleich/new_yearify/pkg/out"
+	"github.com/gleich/new_yearify/pkg/clone"
 )
 
 func main() {
-	PAT := out.Ask("What is your PAT (personal access token)?")
-	if PAT == "" || !strings.HasPrefix(PAT, "ghp_") {
-		lumber.FatalMsg("Please enter a valid response")
-	}
+	// PAT := out.Ask("What is your PAT (personal access token)?")
+	// if PAT == "" || !strings.HasPrefix(PAT, "ghp_") {
+	// 	lumber.FatalMsg("Please enter a valid response")
+	// }
 
-	client := api.Client(PAT)
+	// client := api.Client(PAT)
 
-	username, err := api.Username(client)
+	// username, err := api.Username(client)
+	// if err != nil {
+	// 	lumber.Fatal(err, "Failed to get user's username")
+	// }
+
+	// repos, err := api.Repos(username, client)
+	// if err != nil {
+	// 	lumber.Fatal(err, "Failed to load repos")
+	// }
+	// fmt.Println(repos)
+
+	tmpDir, err := clone.CreateTmpDir()
 	if err != nil {
-		lumber.Fatal(err, "Failed to get user's username")
+		lumber.Fatal(err, "Failed to create temp directory for cloning")
 	}
-
-	repos, err := api.Repos(username, client)
-	if err != nil {
-		lumber.Fatal(err, "Failed to load repos")
-	}
-	fmt.Println(repos)
+	fmt.Println(tmpDir)
 }
---
+++ pkg/clone/dir.go
@@ -0,0 +1,17 @@
+package clone
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func CreateTmpDir() (string, error) {
+	loc := filepath.Join(os.TempDir(), "new_yearify")
+	if _, err := os.Stat(loc); err != nil {
+		err := os.Mkdir(loc, 0755)
+		if err != nil {
+			return "", err
+		}
+	}
+	return loc, nil
+}
